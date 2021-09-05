afe6b17200fe3e8e05e4be2d8ed9673626b139fb
laurettemongina <monginalaurette2gmail.com>
2021-09-05 17:07:20 +0300

    Creates a class Users.

---
+++ users.py
@@ -0,0 +1,12 @@
+
+class Users:
+  """
+  class that generates new instance of user
+  """
+  users_list = []
+
+  
+
+  def __init__(self,username,password):
+    self.username = username
+    self.password = password
