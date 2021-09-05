f02e562dce18499e0ce0b43082c253f23a8341c2
laurettemongina <monginalaurette2gmail.com>
2021-09-05 17:29:43 +0300

    Adds a delete method to delete a user.

--- users.py
+++ users.py
@@ -8,6 +8,12 @@ class Users:
   def save_users(self):
       Users.users_list.append(self)
 
+  def delete_users(self):
+        """
+        deletes a saved user from the users list
+        """ 
+        Users.users_list.remove(self)
+
   def __init__(self,username,password):
     self.username = username
     self.password = password
