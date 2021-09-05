2ccb1605cf429b52f45281f949bba70cbd64940a
laurettemongina <monginalaurette2gmail.com>
2021-09-05 17:10:24 +0300

    Adds a test for save users.

--- users_test.py
+++ users_test.py
@@ -15,7 +15,15 @@ class TestUsers(unittest.TestCase):
     self.assertEqual(self.new_users.username,"laurah52")
     self.assertEqual(self.new_users.password,"0345")
 
-  
+  def test_save_users(self):
+        '''
+        test_save_users test case to test if the users object is saved into
+         the users list
+        '''
+        self.assertEqual(len(Users.users_list),0)
+        self.new_users.save_users()
+        self.assertEqual(len(Users.users_list),1)
+
   
         
 
