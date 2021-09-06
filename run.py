27335b6f7c26653b8a38f4cdd48d1561cc430951
laurettemongina <monginalaurette2gmail.com>
2021-09-06 09:55:16 +0300

    Creates functions to save and create user and credentials.

---
+++ run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3.8
+from users import Users
+from users import Credentials
+
+def create_new_user(username,password):
+    '''
+    Function to create a new user with a username and password
+    '''
+    new_user = Users(username,password)
+    return new_user
+
+def save_user(users):
+    '''
+    Function to save a new user
+    '''
+    users.save_users()
+
+
+def create_new_credential(account,username,password):
+    """
+    Function that creates new credentials
+    """
+    new_credential = Credentials(account,username,password)
+    return new_credential
+
+def login_users(username,password):
+    """
+    function that checks whether a user exist and then login the user in.
+    """
+  
+    check_user = Credentials.verify_users(username,password)
+    return check_user
+
+def save_credentials(credentials):
+    """
+    Function to save Credentials to the credentials list
+    """
+    credentials. save_credentials()
+
+
